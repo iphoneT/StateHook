68f9620650f75efc098722fbc07c20d00b4d7bf0
androidtrivedi1 <android1@trivediinfotech.com>
2021-07-14 11:59:41 +0530

    commit

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ function App() {
   const [count, setCount]=useState(1)
   return (
     <View>
-    <Text style={{fontSize: 80, margin: 50}}> {count} </Text>
+    <Text style={{fontSize: 40, margin: 50}}> {count} </Text>
     <Button title="+ Count" onPress= {()=> setCount(count+1)}/>
     <Button title="- Count" onPress= {()=> setCount(count-1)}/>
     </View>
