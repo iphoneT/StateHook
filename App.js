6b34c8bec324123e4e3461a8cdb2538650aa7d0b
androidtrivedi1 <android1@trivediinfotech.com>
2021-07-14 12:00:57 +0530

    commit

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import { View,Text,Button } from 'react-native';
 function App() {
   const [count, setCount]=useState(1)
   return (
-    <View>
+    <View>s
     <Text style={{fontSize: 40, margin: 50}}> {count} </Text>
     <Button title="+ Count" onPress= {()=> setCount(count+1)}/>
     <Button title="- Count" onPress= {()=> setCount(count-1)}/>
